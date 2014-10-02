a1dd9833ca104f30d62ca4d98ccff5fbdf387e88
William Dizon <wdchromium@gmail.com>
2014-10-02 08:28:22 -0700

    further cleanup to reduce callback hell

--- server.js
+++ server.js
@@ -14,33 +14,36 @@ server.backend = function(socket_emitter) {
   self.front_end = socket_emitter || new events.EventEmitter();
 
   self.create_game = function(dwarf_controller, troll_controller, callback) {
+    /* callback returns function(game_id) */
     var game_id = uuid.v1();
     self.db.games.save({
       game_id: game_id,
-      dwarf_controller: dwarf_controller,
-      troll_controller: troll_controller,
+      dwarf_controller: dwarf_controller || 'human',
+      troll_controller: troll_controller || 'human',
       moves: [],
       starting_positions: STARTING_POSITIONS['CLASSIC'],
       complete: false
     }, function(err, saved) {
-      if (!err)
-        callback(game_id);
-      else
+      if (err) {
         callback(null);
+      } else {
+        callback(saved);
+      }
     })
   }
 
   self.find_game = function(game_id, callback) {
-    self.db.games.findOne({game_id: game_id}, function(err, games) {
+    /* callback returns function(game) */
+    self.db.games.findOne({game_id: game_id}, function(err, game) {
       if (!err)
-        callback(games);
+        callback(game);
       else
         console.error('No game found with', criteria);
     })
   }
 
-  self.turn_to_act = function(game_obj) {
-    return (game_obj.moves.length % 2 == 0 ? 'dwarf' : 'troll')
+  self.turn_to_act = function(moves) {
+    return (moves.length % 2 == 0 ? 'dwarf' : 'troll')
   }
 
   self.query = function(moves, type, callback) {
@@ -72,13 +75,7 @@ server.backend = function(socket_emitter) {
           callback(true);
         })
       } else {
-        console.log('Game:', game_id, 'discarded invalid move:', move)
-        self.db.games.findAndModify({
-          query: {game_id: game_id},
-          update: { $pop: {moves: 1} }
-        }, function(err) {
-          callback(false);
-        })
+        callback(false);
       }
     }
 
@@ -93,35 +90,88 @@ server.backend = function(socket_emitter) {
     console.log('New client connected:', ip);
 
     socket.on('create_game', function(data) {
-      self.create_game(data.dwarf_controller, data.troll_controller, function(game_id) {
-        console.log('Request to provision a new gameboard from', ip);
-        console.log('Game ID:', game_id)
-        socket.emit('new_game_created', {
-          game_id: game_id,
-          positions: STARTING_POSITIONS['CLASSIC']
-        })
+      self.create_game(data.dwarf_controller, data.troll_controller, function(game) {
+        if (!game) {
+          console.log('Failed to provision a new gameboard for', ip);
+        } else {
+          console.log('Request to provision a new gameboard from', ip);
+          console.log('Game ID:', game.game_id)
+          socket.emit('new_game_created', {
+            game_id: game.game_id,
+            positions: game.starting_positions
+          })
+        }
+          
       })
+
     })
 
     socket.on('wait_for_cpu', function(game_id) {
-      self.find_game(game_id, function(game) {
-        if (self.turn_to_act(game) == 'troll' && 
-            game.troll_controller == 'cpu') {
-          self.query(game.moves, 'next_move', function(next_move) {
-            self.append_move(game_id, next_move, function(success) {
-              console.log('Game:', game_id, 'responded with', next_move, 'to', ip);
-              socket.emit('cpu_response', {
-                game: game_id,
-                responded: next_move
-              })
-            });
+      function append_cpu_move(game) {
+        self.query(game.moves, 'next_move', function(next_move) {
+          self.append_move(game.game_id, next_move, function() {
+            console.log('Game:', game_id, 'responded with', next_move, 'to', ip);
+            socket.emit('cpu_response', {
+              game: game_id,
+              responded: next_move
+            })
           })
+        })
+      }
+      self.find_game(game_id, function(game) {
+        if (self.turn_to_act(game.moves) == 'troll' && 
+            game.troll_controller == 'cpu') {
+          append_cpu_move(game);
         }
       })
     })
 
     socket.on('attempt_move', function(data) {
-      self.find_game(data.game_id, function(game) {
+      function pop_last_move() {
+        self.db.games.findAndModify({
+          query: {game_id: data.game_id},
+          update: { $pop: {moves: 1}}
+        })
+      }
+
+      function append_to_moves(game) {
+        self.append_move(data.game_id, data.move, function(success) {
+          if (success) {
+            switch (data.move[0]) {
+              case 'T':
+                self.query(game.moves, 'captures', function(full_capstring) {
+                  if (full_capstring.length > data.move.length) 
+                    pop_last_move();
+                  else
+                    full_capstring = data.move;
+
+                  console.log('Game:', data.game_id, 'accepted move', full_capstring, 'from', ip);
+                  socket.emit('move_accepted', {
+                    game_id: data.game_id,
+                    requested: full_capstring
+                  })
+                })
+                break;
+              case 'd':
+                console.log('Game:', data.game_id, 'accepted move', data.move, 'from', ip);
+                socket.emit('move_accepted', {
+                  game_id: data.game_id,
+                  requested: data.move
+                })
+                break;
+              default:
+                console.error('invalid move token:', data.move[0]);
+            }
+          } else {
+            console.log('Game:', data.game_id, 'rejected move', data.move, 'from', ip);
+          }
+        })
+
+      }
+
+      self.find_game(data.game_id, append_to_moves);
+
+      /*self.find_game(data.game_id, function(game) {
         self.append_move(data.game_id, data.move, function(success) {
           if (success) {
             if (data.move[0] == 'T') {
@@ -156,7 +206,7 @@ server.backend = function(socket_emitter) {
           }
 
         })
-      })
+      })*/
     })
   })
 
